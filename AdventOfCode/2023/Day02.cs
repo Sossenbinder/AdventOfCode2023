2dbe79c82ae0cab6cc5631715d1b7b3de93ac4a0
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2023-12-02 11:31:08 +0100

    - Day 2

--- AdventOfCode/2023/Day02.cs
+++ AdventOfCode/2023/Day02.cs
@@ -6,11 +6,91 @@ public class Day02 : AdventBase
 {
     protected override object InternalPart1()
     {
-        throw new NotImplementedException();
+        var gameSum = 0;
+        
+        const int maxRed = 12;
+        const int maxGreen = 13;
+        const int maxBlue = 14;
+        
+        for (var game = 0; game < Input.Lines.Length; ++game)
+        {
+            var combinations = GetCubesOfLine(Input.Lines[game]);
+
+            if (combinations.Any(combination => combination is not { Blue: <= maxBlue, Green: <= maxGreen, Red: <= maxRed }))
+            {
+                gameSum += game + 1;
+            }
+        }
+
+        return gameSum;
     }
 
     protected override object InternalPart2()
     {
-        throw new NotImplementedException();
+        var gameSum = 0;
+        
+        for (var game = 0; game < Input.Lines.Length; ++game)
+        {
+            var combinations = GetCubesOfLine(Input.Lines[game]);
+
+            var maxRed = combinations.Max(x => x.Red);
+            var maxGreen = combinations.Max(x => x.Green);
+            var maxBlue = combinations.Max(x => x.Blue);
+            gameSum += maxRed * maxGreen * maxBlue;
+        }
+
+        return gameSum;
+    }
+
+    private unsafe List<(int Blue, int Red, int Green)> GetCubesOfLine(string line)
+    {
+        var lineSpan = line.AsSpan();
+
+        var noGamePrefixSpan = lineSpan[(lineSpan.IndexOf(':') + 1)..];
+
+        Span<Range> gameRangesSpan = stackalloc Range[6];
+        var gameSpansAvailable = noGamePrefixSpan.Split(gameRangesSpan, ';');
+
+        var cubeCombinations = new List<(int Blue, int Red, int Green)>();
+        for (var i = 0; i < gameSpansAvailable; ++i)
+        {
+            var gameRange = gameRangesSpan[i];
+            var actualGame = noGamePrefixSpan[gameRange];
+
+            Span<Range> cubeSections = stackalloc Range[3];
+            var cubeSectionsCount = actualGame.Split(cubeSections, ',');
+
+            var blue = 0;
+            var red = 0;
+            var green = 0;
+            foreach (var cubeSection in cubeSections[..cubeSectionsCount])
+            {
+                Span<Range> result = stackalloc Range[2];
+                var game = actualGame[cubeSection].Trim();
+                game.Split(result, ' ');
+
+                var amountPart = game[result[0]].Trim();
+                var color = game[result[1]].Trim();
+
+                var amount = int.Parse(amountPart);
+
+                switch (color[0])
+                {
+                    case 'b':
+                        blue += amount;
+                        break;
+                    case 'r':
+                        red += amount;
+                        break;
+                    case 'g':
+                        green += amount;
+                        break;
+                }
+            }
+            
+            cubeCombinations.Add((blue, red, green));
+        }
+
+        return cubeCombinations;
     }
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -2,7 +2,7 @@
 using AdventOfCodeSupport;
 
 var solutions = new AdventSolutions();
-var today = solutions.GetMostRecentDay();
+var today = solutions.GetDay(2023, 2);
 
 var benchmark = false;
 #if RELEASE
@@ -15,6 +15,8 @@ RunToday(today, benchmark);
 static void RunToday(AdventBase today, bool benchmark = false)
 {
     static string WithPreciseFormat(TimeSpan timeSpan) => $"{timeSpan.Seconds}s {timeSpan.Milliseconds}ms {timeSpan.Microseconds}us {timeSpan.Nanoseconds}us ";
+
+    today.OnLoad();
     
     var sw = Stopwatch.StartNew();
     today.Part1();
@@ -22,7 +24,7 @@ static void RunToday(AdventBase today, bool benchmark = false)
     Console.WriteLine($"Elapsed: {WithPreciseFormat(firstPart)}");
     sw.Restart();
     today.Part2();
-    var secondPart = sw.Elapsed;
+    var secondPart = sw.Elapsed; 
     Console.WriteLine($"Elapsed: {WithPreciseFormat(secondPart)}");
 
     if (benchmark)
