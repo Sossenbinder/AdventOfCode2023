91930f9dd9e7747905ad1fb7efd8a97c9c14feb8
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2023-12-10 16:36:01 +0100

    - Day 09

--- AdventOfCode/2023/Day09.cs
+++ AdventOfCode/2023/Day09.cs
@@ -6,11 +6,32 @@
 	{
 	    protected override object InternalPart1()
 	    {
-	        throw new NotImplementedException();
+		    return Input.Lines
+			    .Select(line => CalculateRowContinuation(line.Split(' ').Select(int.Parse).ToArray()))
+			    .Sum();
 	    }
 
 	    protected override object InternalPart2()
 	    {
-	        throw new NotImplementedException();
+		    return Input.Lines
+			    .Select(line => CalculateRowContinuation(line.Split(' ').Select(int.Parse).Reverse().ToArray()))
+			    .Sum();
+	    }
+
+	    private static int CalculateRowContinuation(IReadOnlyList<int> line)
+	    {
+		    if (line.Sum() == 0)
+		    {
+			    return 0;
+		    }
+
+		    var deltaLine = new int[line.Count - 1];
+
+		    for (var i = 0; i < line.Count - 1; ++i)
+		    {
+			    deltaLine[i] = line[i + 1] - line[i];
+		    }
+
+		    return line[^1] + CalculateRowContinuation(deltaLine);
 	    }
 	}
