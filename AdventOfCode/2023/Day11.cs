15abf586dd15e32bfecefb1529877571db8290e5
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2023-12-11 22:59:59 +0100

    - D11 Part 2

--- AdventOfCode/2023/Day11.cs
+++ AdventOfCode/2023/Day11.cs
@@ -7,21 +7,27 @@
 	    protected override object InternalPart1()
 	    {
 		    var original = Input.Lines.Select(x => x.ToCharArray()).ToArray();
-		    var expanded = Expand(original);
+		    var (xExpansions, yExpansions) = Expand(original);
 		    
-		    var galaxyPoints = expanded
+		    var galaxyPoints = original
 			    .Select((row, y) => row.Select((_, x) => new Point(x, y)))
 			    .SelectMany(x => x)
-			    .Where(x => expanded[x.Y][x.X] == '#')
+			    .Where(x => original[x.Y][x.X] == '#')
 			    .ToList();
 
-		    var totalDistance = 0;
+		    long totalDistance = 0;
 		    for (var i = 0; i < galaxyPoints.Count; i++)
 		    {
 			    var currentGalaxyPoint = galaxyPoints[i];
+			    
+			    var xExtension = xExpansions.Count(x => x < currentGalaxyPoint.X);
+			    var yExtension = yExpansions.Count(y => y < currentGalaxyPoint.Y);
+			    currentGalaxyPoint.X += xExtension;
+			    currentGalaxyPoint.Y += yExtension;
+			    
 			    var targets = galaxyPoints[(i+ 1)..];
 
-			    totalDistance += targets.Sum(x => FindManhattanDistance(currentGalaxyPoint, x));
+			    totalDistance += targets.Sum(x => FindManhattanDistance(currentGalaxyPoint, x, xExpansions, yExpansions));
 		    }
 
 		    return totalDistance;
@@ -29,14 +35,46 @@
 
 	    protected override object InternalPart2()
 	    {
-		    return 0;
+		    var scale = 1_000_000 - 1;
+		    
+		    var original = Input.Lines.Select(x => x.ToCharArray()).ToArray();
+		    var (xExpansions, yExpansions) = Expand(original);
+		    
+		    var galaxyPoints = original
+			    .Select((row, y) => row.Select((_, x) => new Point(x, y)))
+			    .SelectMany(x => x)
+			    .Where(x => original[x.Y][x.X] == '#')
+			    .ToList();
+
+		    long totalDistance = 0;
+		    for (var i = 0; i < galaxyPoints.Count; i++)
+		    {
+			    var currentGalaxyPoint = galaxyPoints[i];
+			    
+			    var xExtension = xExpansions.Count(x => x < currentGalaxyPoint.X);
+			    var yExtension = yExpansions.Count(y => y < currentGalaxyPoint.Y);
+			    currentGalaxyPoint.X += xExtension * scale;
+			    currentGalaxyPoint.Y += yExtension * scale;
+			    
+			    var targets = galaxyPoints[(i+ 1)..];
+
+			    totalDistance += targets.Sum(x => FindManhattanDistance(currentGalaxyPoint, x, xExpansions, yExpansions, scale));
+		    }
+
+		    return totalDistance;
 	    }
 
-	    private int FindManhattanDistance(
+	    private long FindManhattanDistance(
 		    Point start,
-		    Point end)
+		    Point end,
+		    HashSet<long> xExpansions,
+		    HashSet<long> yExpansions,
+		    int scale = 1)
 	    {
-		    return Math.Abs(end.X - start.X) + Math.Abs(end.Y - start.Y);
+		    var xExtension = xExpansions.Count(x => x < end.X);
+		    var yExtension = yExpansions.Count(y => y < end.Y);
+		    
+		    return Math.Abs(end.X + (xExtension * scale) - start.X) + Math.Abs(end.Y + (yExtension * scale) - start.Y);
 	    }
 	    
 	    private static void PrintArr(IReadOnlyList<char[]> field)
@@ -48,11 +86,9 @@
 		    }
 	    }
 
-	    private char[][] Expand(char[][] original)
+	    private (HashSet<long> XExpansions, HashSet<long> YExpansions) Expand(char[][] original)
 	    {
-		    var expandableList = new List<List<char>>();
-
-		    var expandXs = new HashSet<int>();
+		    var expandXs = new HashSet<long>();
 		    for (var x = original[0].Length - 1; x > 0; --x)
 		    {
 			    if (original.All(row => row[x] == '.'))
@@ -60,7 +96,7 @@
 				    expandXs.Add(x);
 			    }
 		    }
-		    var expandYs = new HashSet<int>();
+		    var expandYs = new HashSet<long>();
 		    for (var y = 0; y < original.Length; ++y)
 		    {
 			    if (original[y].All(item => item == '.'))
@@ -69,27 +105,7 @@
 			    }
 		    }
 
-		    var newMaxLength = original[0].Length + expandXs.Count;
-
-		    for (var y = 0; y < original.Length; ++y)
-		    {
-			    var row = original[y].ToList();
-
-			    foreach (var expandX in expandXs)
-			    {
-				    row.Insert(expandX, '.');
-			    }
-
-			    if (expandYs.Contains(y))
-			    {
-				    expandableList.Add(new string('.', newMaxLength).ToList());
-			    }
-			    
-			    expandableList.Add(row);
-		    }
-
-
-		    return expandableList.Select(x => x.ToArray()).ToArray();
+		    return (expandXs, expandYs);
 	    }
 
 	    record struct Point(
